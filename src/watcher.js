3e80215f40f3a749a9f2ca8ad330986ddb80729d
t.ashula <office@ashula.info>
2012-03-21 02:28:44 +0900

    fixes #1

--- src/watcher.js
+++ src/watcher.js
@@ -1,11 +1,11 @@
 var url = 'http://puchi.co/phantom/test.html';
 var page = new WebPage();
-/*page.onError = function (msg, trace) {
+page.onError = function (msg, trace) {
   console.log(msg);
   trace.forEach(function(item) {
     console.log('  ', item.file, ':', item.line);
   });
-};*/
+};
 page.open(url, function(status){
   if ( status !== 'success' ) {
     console.log('something wrong');
@@ -74,19 +74,63 @@ page.open(url, function(status){
         }
 
         function getText( E ){
-          return E.nodeValue.replace(/^\s+ */g, '');
+          return E.nodeValue.replace(/^\s+$/g, '');
         }
         
         function getAttrs( E ){
-          var attrs = E.attributes, attr, val, ret = [], i;
+          var attrs = E.attributes, attr, val, ret = [], i, tag = E.localName, name;
           for ( i = 0; attr = attrs[i]; ++i ){
-            val = E.getAttribute( attr.name );
-            ret.push( { 'name' : attr.name, 'val' : val } );
+            name = attr.name;
+            val = E.getAttribute( name );
+            if ( isUriAttribute( tag, name ) ) {
+              val = getAbsUrl( name, val );
+            }
+            ret.push( { 'name' : name, 'val' : val } );
           }
           return ret;
         }
+
+        function isUriAttribute( tag, aname ){
+          /*
+           * http://dev.w3.org/html5/spec/section-index.html#attributes-1
+           * http://www.w3.org/TR/html401/index/attributes.html
+           */
+          var uriAttrs = {
+            'action'     : ['form'],
+            'background' : ['body'],
+            'cite'       : ['blockquote','q','del','ins'],
+            'classid'    : ['object'],
+            'codebase'   : ['object','applet'],
+            'data'       : ['object'],
+            'formaction' : ['button','input'],
+            'href'       : ['a','area','link','base'],
+            'icon'       : ['command'],
+            'longdesc'   : ['img','frame','iframe'],
+            'manifest'   : ['html'],
+            'poster'     : ['video'],
+            'profile'    : ['head'],            
+            'src'        : ['script','input','frame','iframe','img','video','audio','embed','source','track'],
+            'usemap'     : ['img','input','object']
+          };
+          var attr;
+          tag = tag.toLowerCase();
+          aname = aname.toLowerCase();
+          for (attr in uriAttrs) if (uriAttrs.hasOwnProperty( attr )){
+            if ( aname === attr ){
+              return uriAttrs[attr].some(function(t){ return t === tag; });
+            }
+          }
+          return false;
+        }
+
+        function getAbsUrl( name, val ) {
+          var a = D.createElement( 'a' );
+          a.href = val;
+          return a.href;
+        }
         
         return {
+          'url'      : D.location.href,
           'doctype'  : getDoctype( D ),
           'elements' : getElements( D.documentElement )
         };
@@ -94,7 +138,6 @@ page.open(url, function(status){
       
       return convert(document) ;
     });
-    dom.url = url;
     console.log(JSON.stringify(dom));
   }
   phantom.exit();  
