f3fb228df1971f01bdca28ab9dec126d3ea31639
t.ashula <office@ashula.info>
2012-03-30 23:56:28 +0900

    js-beatified

--- src/watcher.js
+++ src/watcher.js
@@ -1,7 +1,7 @@
-function userAgent( opts ) {
+function userAgent(opts) {
   this.def = {};
-  if ( opts ) {      
-    for( var k in opts ) {
+  if (opts) {
+    for (var k in opts) {
       this.def[k] = opts[k];
     }
   }
@@ -9,210 +9,249 @@ function userAgent( opts ) {
   this.dom = {};
   this.init();
 }
-userAgent.prototype.run = function( url ){
+userAgent.prototype.run = function (url) {
   this.url = url;
   this.page.open(this.url);
-} ;
-userAgent.prototype.init = function(){
+};
+userAgent.prototype.init = function () {
   var ua = this;
   ua.loading = 0;
   ua.page = new WebPage();
-  ua.page.onLoadStarted = function(){
+  ua.page.onLoadStarted = function () {
     //console.log("onLoadStarted");
-    ua.loading ++;
+    ua.loading++;
   };
   ua.page.onError = function (msg, trace) {
     //console.log(msg); trace.forEach(function(item) {  console.log('  ', item.file, ':', item.line);  });
     ua.loading--;
     phantom.exit();
   };
-  ua.page.onLoadFinished = function(status) {
+  ua.page.onLoadFinished = function (status) {
     //console.log(status + ":" + ua.url);
-    ua.dom = ua.page.evaluate(function(){
+    ua.dom = ua.page.evaluate(function () {
       function convert(D) {
         var crossFrames = [];
-        function getDoctype (D) {
-          var doctype = D.doctype, code = '';
+
+        function getDoctype(D) {
+          var doctype = D.doctype,
+            code = '';
           if (doctype) {
-            code = '<!DOCTYPE ' + doctype.nodeName +
-              (doctype.publicId ? ' PUBLIC "' + doctype.publicId + '"' : '') +
-              (doctype.systemId ? ' "' + doctype.systemId + '"' : '') + '>';
+            code = '<!DOCTYPE ' + doctype.nodeName + (doctype.publicId ? ' PUBLIC "' + doctype.publicId + '"' : '') + (doctype.systemId ? ' "' + doctype.systemId + '"' : '') + '>';
           }
           return code;
         }
-        function isUriAttribute( tag, aname ){
+
+        function isUriAttribute(tag, aname) {
           var uriAttrs = {
-            'action'     : ['form'],
-            'background' : ['body'],
-            'cite'       : ['blockquote','q','del','ins'],
-            'classid'    : ['object'],
-            'codebase'   : ['object','applet'],
-            'data'       : ['object'],
-            'formaction' : ['button','input'],
-            'href'       : ['a','area','link','base'],
-            'icon'       : ['command'],
-            'longdesc'   : ['img','frame','iframe'],
-            'manifest'   : ['html'],
-            'poster'     : ['video'],
-            'profile'    : ['head'],            
-            'src'        : ['script','input','frame','iframe','img','video','audio','embed','source','track'],
-            'usemap'     : ['img','input','object']
+            'action': ['form'],
+            'background': ['body'],
+            'cite': ['blockquote', 'q', 'del', 'ins'],
+            'classid': ['object'],
+            'codebase': ['object', 'applet'],
+            'data': ['object'],
+            'formaction': ['button', 'input'],
+            'href': ['a', 'area', 'link', 'base'],
+            'icon': ['command'],
+            'longdesc': ['img', 'frame', 'iframe'],
+            'manifest': ['html'],
+            'poster': ['video'],
+            'profile': ['head'],
+            'src': ['script', 'input', 'frame', 'iframe', 'img', 'video', 'audio', 'embed', 'source', 'track'],
+            'usemap': ['img', 'input', 'object']
           };
           var attr;
           tag = tag.toLowerCase();
           aname = aname.toLowerCase();
-          for ( attr in uriAttrs) if (uriAttrs.hasOwnProperty( attr )){
-            if ( aname === attr ){
-              return uriAttrs[attr].some(function(t){ return t === tag; });
+          for (attr in uriAttrs) if (uriAttrs.hasOwnProperty(attr)) {
+            if (aname === attr) {
+              return uriAttrs[attr].some(function (t) {
+                return t === tag;
+              });
             }
           }
           return false;
         }
-        function getElements( E ) {
-          var TYPE = 'type', NAME = 'name', ATTR = 'attr', CHILD = 'child', VALUE = 'val', ele = {};
-          switch (E.nodeType){
-           case Node.ELEMENT_NODE:
-            ele = (function(E){
-              var children = E.childNodes, child, i, cs = [], c, ele = {}, ats;
-              if (E.hasChildNodes()){
-                for ( i = 0; (child = children[ i ]); ++i ){
-                  c = getElements( child ) ;
-                  if ( c[TYPE] === 'text' && c[VALUE] === '' ){
+
+        function getElements(E) {
+          var TYPE = 'type',
+            NAME = 'name',
+            ATTR = 'attr',
+            CHILD = 'child',
+            VALUE = 'val',
+            ele = {};
+          switch (E.nodeType) {
+          case Node.ELEMENT_NODE:
+            ele = (function (E) {
+              var children = E.childNodes,
+                child, i, cs = [],
+                c, ele = {},
+                ats;
+              if (E.hasChildNodes()) {
+                for (i = 0;
+                (child = children[i]); ++i) {
+                  c = getElements(child);
+                  if (c[TYPE] === 'text' && c[VALUE] === '') {
                     continue;
                   }
-                  cs.push( c );
+                  cs.push(c);
                 }
               }
               if (E.localName.match(/iframe/i)) {
                 try {
                   ele.inner = convert(E.contentDocument);
-                } catch (x) {
-                  if ( E.src && E.src.match(/^http/i) ) {
-                    crossFrames.push( E.src );                    
+                }
+                catch (x) {
+                  if (E.src && E.src.match(/^http/i)) {
+                    crossFrames.push(E.src);
                   }
                   console.log(x);
                 }
               }
               ele[TYPE] = 'element';
               ele[NAME] = E.localName;
-              if ( (ats = getAttrs( E ) ) && ats.length !== 0 ) { ele[ATTR] = getAttrs( E ); }
-              if ( cs && cs.length !== 0 ){ ele[CHILD] = cs; }
+              if ((ats = getAttrs(E)) && ats.length !== 0) {
+                ele[ATTR] = getAttrs(E);
+              }
+              if (cs && cs.length !== 0) {
+                ele[CHILD] = cs;
+              }
               return ele;
             })(E);
             break;
-           case Node.TEXT_NODE:
-            ele = (function(E){
+          case Node.TEXT_NODE:
+            ele = (function (E) {
               var ele = {};
               ele[TYPE] = 'text';
-              ele[VALUE] = getText( E );
+              ele[VALUE] = getText(E);
               return ele;
             })(E);
             break;
-           case Node.CDATA_SECTION_NODE :
-            ele = (function(E){
+          case Node.CDATA_SECTION_NODE:
+            ele = (function (E) {
               var ele = {};
               ele[TYPE] = 'cdata';
-              ele[VALUE] = getText( E );
+              ele[VALUE] = getText(E);
               return ele;
-            })(E);    
+            })(E);
             break;
-           case Node.COMMENT_NODE:
-            ele = (function(E){
+          case Node.COMMENT_NODE:
+            ele = (function (E) {
               var ele = {};
               ele[TYPE] = 'comment';
-              ele[VALUE] = getText( E );
+              ele[VALUE] = getText(E);
               return ele;
             })(E);
             break;
           }
           return ele;
-              
+
         }
 
-        function getText( E ){
+        function getText(E) {
           return E.nodeValue.replace(/^\s+$/g, '');
         }
-        
-        function getAttrs( E ){
-          var attrs = E.attributes, attr, val, ret = [], i, tag = E.localName, name;
-          for ( i = 0; attr = attrs[i]; ++i ){
+
+        function getAttrs(E) {
+          var attrs = E.attributes,
+            attr, val, ret = [],
+            i, tag = E.localName,
+            name;
+          for (i = 0; attr = attrs[i]; ++i) {
             name = attr.name;
-            val = E.getAttribute( name );
-            if ( isUriAttribute( tag, name ) ) {
-              val = getAbsUrl( name, val );
+            val = E.getAttribute(name);
+            if (isUriAttribute(tag, name)) {
+              val = getAbsUrl(name, val);
             }
-            ret.push( { 'name' : name, 'val' : val } );
+            ret.push({
+              'name': name,
+              'val': val
+            });
           }
           return ret;
         }
-        
-        function getAbsUrl( name, val ) {
-          var a = D.createElement( 'a' );
+
+        function getAbsUrl(name, val) {
+          var a = D.createElement('a');
           a.href = val;
           return a.href;
         }
 
-        function getOutofDocument( D ) {
-          var i, d, dc = D.childNodes, ood = [];
-          for ( i = 0; d = dc[i]; ++i ) {
-            switch(d.nodeType) {
-             case Node.DOCUMENT_TYPE_NODE: break;
-             case Node.ELEMENT_NODE: break;
-             case Node.COMMENT_NODE:
-              ood.push({ 'type' : 'comment', 'val' : getText( d )} );
+        function getOutofDocument(D) {
+          var i, d, dc = D.childNodes,
+            ood = [];
+          for (i = 0; d = dc[i]; ++i) {
+            switch (d.nodeType) {
+            case Node.DOCUMENT_TYPE_NODE:
+              break;
+            case Node.ELEMENT_NODE:
+              break;
+            case Node.COMMENT_NODE:
+              ood.push({
+                'type': 'comment',
+                'val': getText(d)
+              });
               break;
             default:
-              ood.push({ 'type' : d.nodeType, 'val' : d.nodeValue });
+              ood.push({
+                'type': d.nodeType,
+                'val': d.nodeValue
+              });
             }
           }
           return ood;
         }
-        
-        function getStyleRule( rule ) {
-          if ( rule.type == 3) {
-            return { 'style' : getStyle( rule.styleSheet ) };
-          } else {
-            return { 'css' : rule.cssText };
+
+        function getStyleRule(rule) {
+          if (rule.type == 3) {
+            return {
+              'style': getStyle(rule.styleSheet)
+            };
+          }
+          else {
+            return {
+              'css': rule.cssText
+            };
           }
         }
-        
-        function getStyle( stylesheet ) {
-          var rules = stylesheet.cssRules, rule, i, rjson = [], ret = {};
-          for ( i = 0; rule = rules[ i ]; ++i ) {
-            rjson.push( getStyleRule( rule ) );
+
+        function getStyle(stylesheet) {
+          var rules = stylesheet.cssRules,
+            rule, i, rjson = [],
+            ret = {};
+          for (i = 0; rule = rules[i]; ++i) {
+            rjson.push(getStyleRule(rule));
           }
           ret['rules'] = rjson;
-          for ( i in stylesheet ) if (stylesheet.hasOwnProperty(i)) {
-            if ( i == 'rules' ) continue;
-            if ( i == 'cssRules' ) continue;
-            if ( i.match(/^owner/i) ) continue;
-            if ( i.match(/^parent/i)) continue;
+          for (i in stylesheet) if (stylesheet.hasOwnProperty(i)) {
+            if (i == 'rules') continue;
+            if (i == 'cssRules') continue;
+            if (i.match(/^owner/i)) continue;
+            if (i.match(/^parent/i)) continue;
             ret[i] = stylesheet[i];
           }
           return ret;
         }
 
-        function getStyles( D ) {
+        function getStyles(D) {
           var i, ss, ssl, ret = [];
-          if ( !D.styleSheets ) {
-          }
+          if (!D.styleSheets) {}
           else {
             ssl = D.styleSheets;
-            for ( i = 0; ss = ssl[ i ]; ++i ) {
-              ret.push( getStyle( ss ) );
+            for (i = 0; ss = ssl[i]; ++i) {
+              ret.push(getStyle(ss));
             }
           }
           return ret;
         }
-        
+
         var domjson = {
-          'url'      : D.location.href,
-          'doctype'  : getDoctype( D ),
-          'elements' : getElements( D.documentElement ),
-          'outofdoc' : getOutofDocument( D ),
-          'styles'   : getStyles( D )
+          'url': D.location.href,
+          'doctype': getDoctype(D),
+          'elements': getElements(D.documentElement),
+          'outofdoc': getOutofDocument(D),
+          'styles': getStyles(D)
         };
-        if ( crossFrames && crossFrames.length !== 0 ) {
+        if (crossFrames && crossFrames.length !== 0) {
           domjson.crosses = crossFrames;
         }
         return domjson;
@@ -220,9 +259,9 @@ userAgent.prototype.init = function(){
       return convert(document);
     });
     console.log(JSON.stringify(ua.dom));
-    phantom.exit();  
+    phantom.exit();
   };
 };
 var url = 'http://ashula.info';
-var phantomUA = new userAgent( {} );
-phantomUA.run( url );
+var phantomUA = new userAgent({});
+phantomUA.run(url);
