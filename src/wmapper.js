c2b80d00ba166b3cfc71945f9f716e91f8392301
t-ashula <t.ashula+github@gmail.com>
2013-12-24 00:29:55 +0900

    separate prototype functions

--- src/wmapper.js
+++ src/wmapper.js
@@ -3,35 +3,40 @@
 'use strict';
 exports = module.exports = WMapper;
 
-function WMapper (opt) {
+function WMapper(opt) {
   this.options = {
-    'agent' : 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.63 Safari/537.36',
-    'loglevel' : 'normal'
+    'agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.63 Safari/537.36',
+    'loglevel': 'normal'
   };
   if (opt) {
-    for ( var k in opt ) {
-      if ( opt.hasOwnProperty( k ) ) {
-        this.options[ k ] = opt[ k ];
+    for (var k in opt) {
+      if (opt.hasOwnProperty(k)) {
+        this.options[k] = opt[k];
       }
     }
   }
 }
 
-WMapper.prototype = {
-  run : function (){
-    console.log('options:' + JSON.stringfiy(this.options));
-  },
-  userAgent : function (ua) {
-    if ( ua !== undefined ) {
-      this.options.agent = ua;
-    }
-    return this.options.agent ;
-  },
-  loglevel : function (level) {
-    if ( ( level === 'verbose' ) || ( level === 'normal' ) ) {
-      this.options.loglevel = level;
-    }
-    
-    return this.options.loglevel;
+WMapper.prototype.run = function () {
+  console.log('options:' + JSON.stringify(this.options));
+};
+
+WMapper.prototype.userAgent = function (ua) {
+  if (ua !== undefined) {
+    this.options.agent = ua;
   }
+
+  return this.options.agent;
+};
+
+WMapper.prototype.loglevel = function (level) {
+  switch (level) {
+  case 'verbose':
+  case 'normal':
+  case 'quiet':
+    this.options.loglevel = level;
+    break;
+  }
+
+  return this.options.loglevel;
 };
