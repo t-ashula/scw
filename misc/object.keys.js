f19a4003f5d48feaaf8ba710c77b4b4ff5445563
t-ashula <t.ashula+github@gmail.com>
2014-01-24 20:33:31 +0900

    output format changed

--- misc/object.keys.js
+++ misc/object.keys.js
@@ -4,16 +4,24 @@ var page = require('webpage').create(),
   objName = system.args[2] || 'window';
 
 page.open(url, function (status) {
-  var res = page.evaluate(function (on) {
-    var o = window, ns = on.split(/\./g);
-    for(var i = 0, n; n = ns[i]; ++i ) {
-      o = o[n];
-      if ( o === undefined ){
-        return [];
+  var res = page.evaluate(function (ons) {
+    Array.prototype.selectMany = function (fn) {
+      return this.map(fn).reduce(function (x, y) { return x.concat(y); }, []);
+    };
+    return ons.split(',').filter(function (on) {
+      return on !== '';
+    }).map(function (on) {
+      var o = window,
+        ns = on.split(/\./g);
+      for (var i = 0, n; n = ns[i]; ++i) {
+        o = o[n];
+        if (o === undefined) {
+          return [];
+        }
       }
-    }
-    return Object.keys(o).sort();
+      return Object.keys(o).sort().map(function(k){ return on + "." + k; } );
+    }).selectMany(function(x){ return x; });    
   }, objName);
-  console.log(JSON.stringify(res));
+  console.log(JSON.stringify(res, null, 2));
   phantom.exit();
 });
